08a5098d3b88d357fd1eb47e08ee3eb1a2f35d1d
Maria Raquel <maria.raquel.roriz@gmail.com>
2023-01-02 16:35:33 -0300

    #22: aulas 209, 210 e 211  completas

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,6 +12,7 @@ type ButtonTypes =
 export type ButtonProps = {
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  minimal?: boolean
   icon?: JSX.Element
   as?: React.ElementType
 } & ButtonTypes
@@ -22,10 +23,17 @@ const Button = ({
   children,
   size = 'medium',
   fullWidth = false,
+  minimal = false,
   icon,
   ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    {...props}
+  >
     {!!icon && icon} {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
