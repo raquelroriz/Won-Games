9aea3af5fad9dfa64b232b59de3d09d83563869a
Maria Raquel <maria.raquel.roriz@gmail.com>
2023-01-09 18:50:04 -0300

    #23: aulas 212 até 224  completas

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,49 @@
+import { useState, InputHTMLAttributes } from 'react'
+
+import * as S from './styles'
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void
+  label?: string
+  labelFor?: string
+  initialValue?: string
+  icon?: React.ReactNode
+  iconPosition?: 'left' | 'right'
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  icon,
+  iconPosition = 'left',
+  label,
+  labelFor = '',
+  initialValue = '',
+  onInput,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInput && onInput(newValue)
+  }
+
+  return (
+    <S.Wrapper>
+      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
+        <S.Input
+          type="text"
+          onChange={onChange}
+          value={value}
+          iconPosition={iconPosition}
+          {...props}
+        />
+      </S.InputWrapper>
+    </S.Wrapper>
+  )
+}
+
+export default TextField
---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components'
+
+import { TextFieldProps } from '.'
+
+type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    background: ${theme.colors.lightGray};
+    border-radius: 0.2rem;
+    padding: 0 ${theme.spacings.xsmall};
+    border: 0.2rem solid;
+    border-color: ${theme.colors.lightGray};
+    &:focus-within {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Input = styled.input<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    color: ${theme.colors.black};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
+    background: transparent;
+    border: 0;
+    outline: none;
+    width: 100%;
+  `}
+`
+export const Label = styled.label`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    cursor: pointer;
+  `}
+`
+
+export const Icon = styled.div<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    display: flex;
+    width: 2.2rem;
+    color: ${theme.colors.gray};
+    order: ${iconPosition === 'right' ? 1 : 0};
+    & > svg {
+      width: 100%;
+    }
+  `}
+`
+export const Wrapper = styled.div``
