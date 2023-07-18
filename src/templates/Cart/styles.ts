69225441b3cc934d214e5ed4fecd79b67ec9674b
Maria Raquel <maria.raquel.roriz@gmail.com>
2023-07-18 11:30:43 -0300

    #29: aulas 298 completa

--- src/templates/Cart/styles.ts
+++ src/templates/Cart/styles.ts
@@ -7,6 +7,7 @@ export const Content = styled.div`
     grid-template-columns: 1fr;
     grid-gap: ${theme.spacings.large};
     margin: ${theme.spacings.large} 0;
+
     ${media.greaterThan('medium')`
       grid-template-columns: 2fr 1fr;
     `}
